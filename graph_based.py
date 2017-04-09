3c1055fc52cfddd02322187409e0fc4ebd9e9951
saurabhcse15 <saurabh.yadav.cse15@iitbhu.ac.in>
2017-04-09 14:08:28 +0530

    Update graph_based.py
    added gaussian filter

--- graph_based.py
+++ graph_based.py
@@ -1,13 +1,12 @@
-import numpy as np
+﻿import numpy as np
 import sys
 import matplotlib.pyplot as plt
 from skimage import io
-#filename=sys.argv[1]
-
+from graph import build_graph, segment_graph
+from smooth_filter import gaussian_grid, filter_image
 from random import randint
 
 def initl( idx, qrank,maxMST,n):
-	#print type(idx),type(qrank)
 	for i in range(0,n):
 		idx.append(i);
 		qrank.append(1);
@@ -38,13 +37,7 @@ def isconnected( idx, u, v):
         return True;
     else:
         return False;
-        
-def printf( arr, n):
-    for i in range(0,n):
-        print arr[i],
-    print
-
-
+      
 #Building-4-connected-graph
 def compute_edges(edges,data,row,col,layer):
 	for i in range(0,row):
@@ -68,8 +61,7 @@ def compute_edges(edges,data,row,col,layer):
 				edges.append([weight,col*(i)+j,col*(i)+j-1 ])
 					
 #computing_segmented_image				
-def graph_based(data,k,m,edges,idx,qrank,maxMST): 
-	minsize=20
+def graph_based(data,k,m,edges,idx,qrank,maxMST,minsize): 
    	n=len(edges)
    	count=0
    	for i in range(0,n):
@@ -107,39 +99,40 @@ def graph_based(data,k,m,edges,idx,qrank,maxMST):
 	for i in range(0,m):
 		data[i][0]=maps1[idx[i]]/maps4[idx[i]]
 		data[i][1]=maps2[idx[i]]/maps4[idx[i]]
-		data[i][2]=maps3[idx[i]]/maps4[idx[i]]
-		
+		data[i][2]=maps3[idx[i]]/maps4[idx[i]]	
 	return data
 	
-    
-#start of main
-
 def graph_based_seg(data,minsize,k,sigma):
 	m=int();idx=list();qrank=list();edges=list();maxMST=list()
 	layer=1
-	print "Graph_based ru+n"
+	print "Graph_based run"
 	row=data.shape[0]
 	col=data.shape[1]
-
 	if len(data.shape)==3:
 		layer=data.shape[2]
-
 	m=row*col
-
+	#Applying_Gaussian_Filter
+	
+	data=data.reshape(m,layer)
+	r=data[:,0];r=r.reshape(m,1)
+	g=data[:,1];g=g.reshape(m,1)
+	b=data[:,2];b=b.reshape(m,1)
+	
+	grid = gaussian_grid(sigma)
+	r=filter_image(r,grid);r=r.reshape(1,m)
+    	g=filter_image(g,grid);g=g.reshape(1,m)
+    	b=filter_image(b,grid);b=b.reshape(1,m)
+    	data[:,0]=r
+	data[:,1]=g
+	data[:,2]=b
+	data=data.reshape(row,col,layer)
+	
 	initl(idx,qrank,maxMST,m)
-
 	compute_edges(edges,data,row,col,layer)
 	edges.sort()
-
 	data=data.reshape(m,layer)
-
-	print "data shapes: ",data.shape[0],data.shape[1]
-	#print "give k"
-	#k=input()
-	data=graph_based(data,k,m,edges,idx,qrank,maxMST)#segmentating
+	
+	data=graph_based(data,k,m,edges,idx,qrank,maxMST,minsize)#segmentating
 	data=data.reshape(row,col,layer)
 	
 	return data 
-
-
-
